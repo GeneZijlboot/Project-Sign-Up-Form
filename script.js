eadb6c066f007c4e4db96cb9686adb499be5eee6
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-05-31 11:44:22 +0200

    final version Sign-Up-Form

---
+++ script.js
@@ -0,0 +1,12 @@
+unction CheckPassword(){
+    let Password = document.getElementById("Password");
+    let ConfirmPassword = document.getElementById("ConfirmPassword");
+    console.log(Password.value, ConfirmPassword.value);
+
+    if(Password.value === ConfirmPassword.value){
+        location.reload(); //refreshes the page when everything is correcty filled in
+    }else{
+        document.getElementById("Password").style.backgroundColor="red";
+        document.getElementById("ConfirmPassword").style.backgroundColor="red";
+    }
+}
